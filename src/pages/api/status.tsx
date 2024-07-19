c4a5cb37852c4408dff63a3a7c70c19676e1f9a8
Faisal Sayed <faisal.sayed502@gmail.com>
2024-07-19 11:05:27 -0400

    fix api to return the latest record

--- src/pages/api/status.tsx
+++ src/pages/api/status.tsx
@@ -11,20 +11,18 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!data.email && !data.slack_id) return res.status(400).json({ message: "Invalid user" });
 
   try {
-    const record = await base("Users")
+    const records = await base("Users")
       .select({
         filterByFormula: `OR({Email} = '${data.email}', {Hack Club Slack ID} = '${data.slack_id}')`,
+        sort: [{ field: "Created At", direction: "desc" }],
       })
-      .firstPage();
+      .all();
 
-    if (record.length > 0) {
-      return res
-        .status(200)
-        .json(
-          `Verification Status for ${data.email || data.slack_id}: ${
-            record.at(-1)?.fields["Verification Status"]
-          }`
-        );
+    if (records.length > 0) {
+      return res.status(200).json({
+        status: records[0].fields["Verification Status"],
+        reason: records[0].fields["Reason (Sent in email)"],
+      });
     } else {
       return res.status(200).json(`User ${data.email || data.slack_id} not found!`);
     }
